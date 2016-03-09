826c76993318df2029773cf81fdaa3ad77a2cb00
ZhangZhenNan <larry.zzn@gmail.com>
2016-03-09 18:04:40 +0800

    Initial Commit

---
+++ zValidator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zValidator
+//
+//  Created by ZhangZhenNan on 16/3/9.
+//  Copyright © 2016年 zhennan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zValidator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  zValidator
+//
+//  Created by ZhangZhenNan on 16/3/9.
+//  Copyright © 2016年 zhennan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
